27f41f3ab8e0a74eece944e5b63afc3b17707172
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2023-07-13 12:08:41 +0900

    Appmain途中

--- matti3/AppMain.cpp
+++ matti3/AppMain.cpp
@@ -14,6 +14,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	}
 
+
 	//入力待機
 	WaitKey();
 
@@ -22,4 +23,62 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	return 0;
 }
+
+/**********************
+*プログラム実習
+:マッチ３のゲーム制作
+**********************/
+
+#include"DxLib.h"
+#include"FreamControl.h"
+#include"InputControl.h"
+#include"SceneManager.h"
+
+/**********************
+*マクロ定義
+**********************/
+
+#define SCREEN_HEIGHT	(480)	//スクリーンサイズ（高さ）
+#define SCREEN_WIDTH	(640)	//スクリーンサイズ（幅）
+#define SCREEN_COLORBIT (32)	//スクリーンカラービット
+#define FONT_SIZE		(20)	//文字サイズ
+
+/**********************
+*型定義
+**********************/
+
+
+
+/**********************
+*グローバル変数宣言
+**********************/
+
+
+
+/**********************
+*プロトタイプ宣言
+**********************/
+
+
+
+/**********************
+*プログラムの開始
+**********************/
+int WINAPI WinMain(_In_HINSTANCE hInstance, _In_opt_HINSTANCE hPrevInstance,
+	_In_LPSTR lpCmdLine, _In_int nShowCmd)
+{
+
+
+	//ウィンドウタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+	//ウィンドウモードで起動
+	ChangeWindowMode(TRUE);
+	//画面サイズの最大サイズ、カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
+	//Dxライブラリ初期化
+
+}
+
+
 
